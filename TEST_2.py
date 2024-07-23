3f1bab5598297d16ac59f56c8d58b885ae843d60
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-23 11:00:14 +0300

    Update TEST_2.py

--- TEST_2.py
+++ TEST_2.py
@@ -8,7 +8,7 @@ try:
         try:
             result.append(int(i))
         except ValueError as ve:  # встретил строку
-            # print(f'Это не число: {ve}!!!! Продолжаем!!!!')
+            print(f'Это не число: {ve}!!!! Продолжаем!!!!')
             continue
 
 except Exception:
